33961202a6353ad1762b9359c59e2989e53b2622
Sunny Lam <sunny.lam@gmail.com>
2015-08-19 16:37:56 -0400

    constructed the Comment List and Comment Form components

--- js/react.script.081915.1.js
+++ js/react.script.081915.1.js
@@ -4,13 +4,25 @@
 * WARNING:  error = react js unexpected token <
 	* https://stackoverflow.com/questions/20905227/reactjs-unexpected-token-error
 * You must insert this file in scripts.jade as a type="text/jsx"
-* 
+*
+* CODE SKELETON
+	* - CommentBox
+		  - CommentList
+		    - Comment
+		  - CommentForm
+	* so the CommentList and CommentForm are children of CommentBox
+	* Comment is a child of CommentList
 */
 
 // ----------------------------------------
-// COMMENT BOX PRACTICE 1  ------------------
+// COMMENT BOX  ------------------
 // ----------------------------------------
-	// tutorial1.js
+	
+	/* 
+	* to make use of the CommentList and CommentForm, insert them into the html framework in CommentBox
+	* 
+	*/
+
 	var CommentBox = React.createClass({
 	  render: function() {
 	    return (
@@ -20,14 +32,48 @@
 	    );
 	  }
 	});
+
+	// first argument is the component to be inserted, the 2nd argument is the target location or insertion point
 	React.render(
 	  <CommentBox />,
 	  document.getElementById('content')
 	);
 // ----------------------------------------
-// END COMMENT BOX PRACTICE 1  ------------------
+// END COMMENT BOX  ------------------
 // ----------------------------------------
 
+
+// ----------------------------------------
+// COMMENT LIST  ------------------
+// ----------------------------------------
+	
+	var CommentList = React.createClass({
+		render: function () {
+			return (
+				<div className="commentList">
+					Hello, world!  I am a CommentList.
+				</div>
+			);
+		}
+	});
+// ----------------------------------------
+// END COMMENT LIST  ------------------
+// ----------------------------------------
+
+
+// ----------------------------------------
+// COMMENT FORM  ------------------
+// ----------------------------------------
+	var CommentForm = React.createClass({
+		return (
+			<div className="commentForm">
+				Hello world!  I am a CommentForm!
+			</div>
+		);
+	});
+// ----------------------------------------
+// END COMMENT FORM  ------------------
+// ----------------------------------------
 
 // ----------------------------------------
 // LIKE OR NOT  ------------------
